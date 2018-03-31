ba3610654e85ff918051f4894826e7889f604283
TalCohen27 <tal.cohen2011@gmail.com>
2018-03-31 20:07:06 +0300

    Added vue.js frontend

--- hello_world.py
+++ hello_world.py
@@ -1,12 +1,22 @@
 import webapp2
+import os
+import jinja2
+import urllib
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 
 class HelloWorldApp(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        # url = self.request.uri 'url': url
+        template = JINJA_ENVIRONMENT.get_template('sp_frontend/dist/index.html')
+        self.response.out.write(template.render({}))
 
 
 hello_world_app = webapp2.WSGIApplication([
     ('/', HelloWorldApp),
 ], debug=True)
+
